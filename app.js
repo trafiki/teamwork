16816ddf1825731e61da9f19de420931110d47a7
Machine <babsake@gmail.com>
2019-11-19 21:40:27 +0100

    install dependencies

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// app.get('/', (req, res) => res.status(200).send({ message: 'YAY! Congratulations! Your first endpoint is working' }));
+
+app.get('/', (req, res) => {
+  res.json({ status: 'success', message: 'Welcome To Testing API' });
+});
+
+app.post('/add', (req, res) => {
+  const { num1, num2 } = req.body;
+  const add = (number1, number2) => number1 + number2;
+  res.json({
+    status: 'success',
+    message: 'Welcome To Testing API',
+    result: add(num1, num2),
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('app running on port ', PORT);
+});
+module.exports = app;
+// npm run dev-start
