4284fc9d8c7a084d2236e53d5184da119bb3846f
Machine <babsake@gmail.com>
2019-12-13 06:50:21 +0100

    ft(user): Admin can create employee

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 
+import userRouter from './routes/user';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/v1', (req, res) => res.status(200).send({ message: 'YAY! Congratulations! Your first endpoint is working' }));
+app.use('/api/v1', userRouter);
+
+app.get('/api/v1', (req, res) => res.status(200).send({ message: 'Welcome to Teamwork, a space that fosters collaboration.' }));
 
 app.listen(PORT, () => {
   console.log('app running on port ', PORT);
